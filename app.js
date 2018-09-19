ed74a51f001895c39dbb3a14e873309073127e70
Kevin Navero <knavero@gmail.com>
2018-09-19 01:16:15 -0700

    app.js: added error handling

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const process = require('process');
 const path = require('path');
 const express = require('express');
 const logger = require('morgan');
+const createError = require('http-errors');
 
 const indexRouter = require('./routes/index');
 const bootRouter = require('./routes/boot');
@@ -18,5 +19,15 @@ app.use(express.json());
 
 app.use('/', indexRouter);
 app.use('/boot', bootRouter);
+
+app.use((req, res, next) => {
+   next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+   err = req.app.get('env') === 'development' ? err : {}
+   res.status(err.status || 500);
+   res.json(err);
+})
 
 module.exports = app;
